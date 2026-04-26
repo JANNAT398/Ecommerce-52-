aaeef58cfddbaa3a180dc2a15027f0c5d20c37eb
JANNAT398 <islamjanat956@gmail.com>
2026-04-26 08:41:40 +0600

    change

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Container = ({Children}) => {
+const Container = ({children}) => {
   return (
      <>
-         <div className='max-w-[1320px] mx-auto bg-red-500'>
-            {Children}
+         <div className='max-w-[1320px] mx-auto'>
+            {children}
          </div>
      </>
   )
