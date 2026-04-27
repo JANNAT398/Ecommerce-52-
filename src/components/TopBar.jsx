33fc00bf16df448a3bf7dd0410aa51d4eb7ac294
JANNAT398 <islamjanat956@gmail.com>
2026-04-27 11:56:51 +0600

    add2

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import Container from './layout/Container'
 import { CiLocationOn } from "react-icons/ci";
+import { FaAngleDown } from "react-icons/fa";
+
 
 
 const TopBar = () => {
   return (
      <>
-        <div className='border border-solid border-b-gry'>
+        <div className='border border-solid border-b-gry font-pop text-[#666666] text-sm'>
             <Container>
               <div className='flex justify-between'>
-                <div> <CiLocationOn />Store Location: Lincoln- 344, Illinois, Chicago, USA</div>
-                <div>dfgd</div>
+                <div className='flex items-center gap-x-1'> <CiLocationOn /> Store Location: Lincoln- 344, Illinois, Chicago, USA</div>
+                <div className='flex gap-x-5'>
+                  <div className='flex items-center'>
+                    Eng 
+                    <FaAngleDown />
+                  </div>
+                  <div className='flex items-center'>
+                    USD
+                    <FaAngleDown />
+                  </div>
+                  <div className='flex items-center relative after::w-[5px] after::h-[5px] after::bg-red-500 after::content-[*]'>
+                  Sign In/Sign Up
+                  </div>
+                </div>
               </div>
             </Container>
         </div>
