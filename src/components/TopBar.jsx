8ac9d50172ae6dd5510bbf3843638c1a09bac2c2
JANNAT398 <islamjanat956@gmail.com>
2026-04-30 10:29:33 +0600

    custom hook manage

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,67 +1,21 @@
-// import React, { useRef } from 'react'
-// import Container from './layout/Container'
-// import { CiLocationOn } from "react-icons/ci";
-// import { FaAngleDown } from "react-icons/fa";
-// const TopBar = () => {
-//   let dropRefLan = useRef(null)
-//   let handleDropLan = () =>{
-//     if(dropRefLan.current.style.display == 'block'){
-//       dropRefLan.current.style.display = 'none'
-//     }else{
-//       dropRefLan.current.style.display = 'block'
-//     }
-//   }
-//   return (
-//      <>
-//         <div className='border border-solid border-b-gry font-pop text-[#666666] text-sm py-[3.5px]'>
-//             <Container>
-//               <div className='flex justify-between'>
-//                 <div className='flex items-center gap-x-1'> <CiLocationOn /> Store Location: Lincoln- 344, Illinois, Chicago, USA</div>
-//                 <div className='flex gap-x-5'>
-//                   <div className='flex items-center' onClick={handleDropLan}>
-//                     Eng 
-//                     <FaAngleDown />
-//                   </div>
-//                   <div ref={dropRefLan} className='absolute top-[35px] bg-gry py-3 hidden'>
-//                     <ul>
-//                       <li>Ba</li>
-//                       <li>CH </li>
-//                     </ul>
-//                   </div>
-//                   <div className='flex items-center'>
-//                     USD
-//                     <FaAngleDown />
-//                   </div>
-//                   <div className='flex items-center relative after:w-[2px] after:h-[15px] after:bg-[#E6E6E6] after:content-[] after:absolute after:top-[2px] after:left-[-10px]'>
-//                   Sign In/Sign Up
-//                   </div>
-//                 </div>
-//               </div>
-//             </Container>
-//         </div>
-//      </>
-//   )
-// }
-
-// export default TopBar
-
-
-
-
-
 import React, { useRef, useState } from "react";
 import Container from "./layout/Container";
 import { CiLocationOn } from "react-icons/ci";
 import { FaAngleDown } from "react-icons/fa";
-import { useOnClickOutside } from "../hooks/useOnClickOutside";
+import { useOutsideClick } from "../hooks/useOutsideClick ";
 
-const TopBar = () => {
-  const [openLan, setOpenLan] = useState(false);
-  const dropRefLan = useRef(null);
+export default function TopBar() {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
+  useOutsideClick(dropdownRef, () => setOpen(false), open);
  
-  useOnClickOutside(dropRefLan, () => setOpenLan(false));
-
+  
+  useOutsideClick(
+    dropdownRef,
+    () => setOpen(false),
+    open
+  )
   return (
     <div className="border border-solid border-b-gry font-pop text-[#666666] text-sm py-[3.5px]">
       <Container>
@@ -71,14 +25,14 @@ const TopBar = () => {
             Store Location: Lincoln- 344, Illinois, Chicago, USA
           </div>
           <div className="flex gap-x-5 items-center">
-            <div className="relative" ref={dropRefLan}>
+            <div className="relative" ref={dropdownRef}>
               <div
                 className="flex items-center"
-                onClick={() => setOpenLan(!openLan)}
+                onClick={() => setOpen(!open)}
               >
                 Eng <FaAngleDown />
               </div>
-              {openLan && (
+              {open && (
                 <div className="absolute top-[25px] bg-gray-200 py-3 px-3">
                   <ul>
                     <li>BA</li>
@@ -100,4 +54,3 @@ const TopBar = () => {
   );
 };
 
-export default TopBar;
